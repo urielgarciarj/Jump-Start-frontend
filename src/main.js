83e9c75b5e3f7056dbdcb6be6a9ab4bbbe6f9900
Vianey Nava <vianey.nava@bringitps.com>
2024-10-12 20:25:27 -0600

    001-Register-User-View 	- Added folder to manage routes 	- Updated view with functional req to backend
    Signed-off-by: Vianey Nava <vianey.nava@bringitps.com>
    
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	new file:   src/assets/css/nucleo-icons.css
    	new file:   src/assets/css/nucleo-svg.css
    	new file:   src/assets/fonts/nucleo-icons.eot
    	new file:   src/assets/fonts/nucleo-icons.svg
    	new file:   src/assets/fonts/nucleo-icons.ttf
    	new file:   src/assets/fonts/nucleo-icons.woff
    	new file:   src/assets/fonts/nucleo-icons.woff2
    	new file:   src/assets/fonts/nucleo.eot

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
+import store from './store'
+import router from './routes'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
---
+++ src/views/RegisterUsers.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="container mt-4">
+	  <h2>Registro de Usuario</h2>
+	  <form @submit.prevent="registrarUsuario">
+		<!-- Nombre -->
+		<div class="form-group">
+		  <label for="input-nombre">Nombre</label>
+		  <input type="text" class="form-control" id="input-nombre" v-model="usuario.name" required>
+		</div>
+  
+		<!-- Email -->
+		<div class="form-group">
+		  <label for="input-email">Email</label>
+		  <input type="email" class="form-control" id="input-email" v-model="usuario.email" required>
+		</div>
+  
+		<!-- Contraseña -->
+		<div class="form-group">
+		  <label for="input-password">Contraseña</label>
+		  <input type="password" class="form-control" id="input-password" v-model="usuario.password" required>
+		</div>
+  
+		<br />
+		<!-- Botón de registro -->
+		<button type="submit" class="btn btn-primary">Registrar</button>
+	  </form>
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      usuario: {
+        name: '',
+        password: '',
+        email: ''
+      },
+      token: 'xyz123'
+    };
+  },
+  methods: {
+    async registrarUsuario() {
+      try {
+        // Enviar los datos al backend
+        const response = await axios.post('http://localhost:3000/users', this.usuario);
+        console.log('Usuario registrado:', response.data);
+      } catch (error) {
+        console.error('Error al registrar el usuario:', error);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
