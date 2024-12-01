7a7fcd7f77605fc201df6cda5d8a1446f434a05c
Uriel Garcia <urielgarciarj@gmail.com>
2024-12-01 14:09:13 -0600

    Template pro added
    Signed-off-by: Uriel Garcia <urielgarciarj@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": "off",
+    "prettier/prettier": ["error", { endOfLine: "off" }],
+    "javascript.validate.enable": false,
+  },
+};
