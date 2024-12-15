64ece981733e0151c99c73379d5fc435cc60e017
Vianey Nava <vianey.nava@bringitps.com>
2024-12-15 15:30:37 -0600

    Posts creation and show the new one

---
+++ src/components/posts/createPostForm.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios, { AxiosError } from 'axios';
+
+const emit = defineEmits<{
+  (e: 'postCreated', newPost: any): void;
+}>();
+const isActive = ref(false);
+const post = ref({
+    title: '',
+    description: '',
+    category: '',
+    dateCreated: '2024-12-15',
+    mediaUrl: 'www.icarly.com',
+    userId: '11'
+});
+
+const valid = ref(false);
+
+// Reglas de validación
+const notEmptyRule = [
+  (value: string) => !!value || 'Es obligatorio llenar este campo.'
+];
+
+const error = ref<string | null>(null);
+
+const createPost = async () => {
+    if (valid.value) {
+        try {
+            const response = await axios.post('http://localhost:3000/posts/create', post.value);
+            console.log('Post created:', response.data);
+            
+            // Emitir el evento con el nuevo post
+            const newPost = response.data;
+            emit('postCreated', newPost);
+            isActive.value = false; // Close dialog
+        } catch (err) {
+            console.error('Error:', err);
+            // Tipar el error como AxiosError
+            const errorAxios = err as AxiosError;
+            // Manejar el error en función del código de estado
+            if (errorAxios.response) {
+                error.value = 'Ocurrió un error inesperado. Intenta nuevamente.';
+            }
+        }
+    }
+};
+</script>
+
+<template>
+    <v-form v-model="valid" @submit.prevent="createPost">
+        <v-dialog v-model="isActive"  transition="dialog-bottom-transition" class="dialog-mw">
+            <template v-slot:activator="{ props }">
+                <v-btn color="primary" class="w-100" v-bind="props" flat>Nueva publicación</v-btn>
+            </template>
+            <template v-slot:default="{ isActive }">
+                <v-card>
+                    <v-toolbar color="primary" class="px-6">Nueva publicación</v-toolbar>
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field v-model="post.title" :rules="notEmptyRule" label="Titulo" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field v-model="post.description" :rules="notEmptyRule" label="Descripción" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-select v-model="post.category" :rules="notEmptyRule"
+                                    :items="['Informativo', 'Software', 'Aviso', 'Producto']"
+                                    label="Categoría"
+                                    required>
+                                </v-select>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-file-input multiple label="Adjuntar" hide-details variant="outlined"> </v-file-input>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                    <v-card-actions class="justify-end">
+                        <v-btn variant="tonal" @click="isActive.value = false" flat>Cancelar</v-btn>
+                        <v-btn color="primary" variant="tonal" @click="createPost" flat>Guardar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </template>
+        </v-dialog>
+    </v-form>
+</template>
--- src/views/StarterPage.vue
+++ src/views/StarterPage.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue';
 
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
-import UiParentCard from '@/components/shared/UiParentCard.vue';
+import PostForm from '@/components/posts/createPostForm.vue';
 import axios from 'axios';
 
 const page = ref({ title: 'Publicaciones' });
@@ -17,70 +17,26 @@ const posts = ref<any[]>([]);
 // Hacer la petición HTTP cuando el componente se monte
 onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:3000/posts/list/all');
+    const response = await axios.get('http://localhost:3000/posts/list/sorted/newest-date');
     posts.value = response.data;
   } catch (error) {
     console.error('Error al obtener los posts:', error);
   }
 });
+// Función para actualizar la lista de posts cuando se crea un nuevo post
+const addNewPost = (newPost: any) => {
+  posts.value.unshift(newPost);
+};
 </script>
 
 <template>
     <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
     <v-row>
-        <v-col cols="12" md="12">
-            <v-dialog transition="dialog-bottom-transition" class="dialog-mw">
-                <template v-slot:activator="{ props }">
-                    <v-btn color="primary" class="w-100" v-bind="props" flat>Nueva publicación</v-btn>
-                </template>
-                <template v-slot:default="{ isActive }">
-                    <v-card>
-                        <v-toolbar color="primary" class="px-6">Nueva publicación</v-toolbar>
-                        <v-card-text>
-                            <v-row>
-                                <v-col cols="12">
-                                <v-text-field label="Titulo" required></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                <v-text-field
-                                    label="Descripción"
-                                    required
-                                ></v-text-field>
-                                </v-col>
-                                <v-col cols="12" sm="6">
-                                <v-select
-                                    :items="['0-17', '18-29', '30-54', '54+']"
-                                    label="Age*"
-                                    required
-                                ></v-select>
-                                </v-col>
-                                <v-col cols="12" sm="6">
-                                <v-autocomplete
-                                    :items="[
-                                    'Skiing',
-                                    'Ice hockey',
-                                    'Soccer',
-                                    'Basketball',
-                                    'Hockey',
-                                    'Reading',
-                                    'Writing',
-                                    'Coding',
-                                    'Basejump',
-                                    ]"
-                                    label="Interests"
-                                    multiple
-                                ></v-autocomplete>
-                                </v-col>
-                            </v-row>
-                        </v-card-text>
-                        <v-card-actions class="justify-end">
-                            <v-btn variant="tonal" @click="isActive.value = false" flat>Cancelar</v-btn>
-                            <v-btn color="primary" variant="tonal" @click="isActive.value = false" flat>Guardar</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </template>
-            </v-dialog>
+        <v-col cols="12" sm="2">
+            <PostForm @postCreated="addNewPost"/>
         </v-col>
+    </v-row>
+    <v-row>
         <v-col v-for="post in posts" :key="post.id" cols="12" md="12">
             <v-card elevation="10">
                 <v-card-item>
