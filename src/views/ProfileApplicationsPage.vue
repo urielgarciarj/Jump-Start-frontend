57268a3c2a8eaef1f1dfc058861e7d9c131d6202
Vianey Nava <vianey.nava@bringitps.com>
2025-02-08 13:11:11 -0600

    fixes on applies y vacants

--- src/views/ProfileApplicationsPage.vue
+++ src/views/ProfileApplicationsPage.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
-import { useAuthStore } from '@/stores/auth';
-import VacantContent from '@/components/vacancies/VacantContent.vue';
 import axios from 'axios';
-
-const authStore = useAuthStore();
-const userId = authStore.userId;
-const userRole = authStore.userRole;
-
-// components
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
+import VacantContent from '@/components/vacancies/VacantContent.vue';
 import ProfileBanner from '@/components/profile/ProfileBanner.vue';
-import IntroCard from '@/components/profile/IntroCard.vue';
 
+const route = useRoute();
+
+const usuarioId = route.params.id;
 // Definir la referencia para los posts
 const vacantsArray = ref<any[]>([]);
 const page = ref({ title: 'Perfil de usuario' });
@@ -24,14 +20,13 @@ const breadcrumbs = ref([
 // Hacer la petición HTTP cuando el componente se monte
 onMounted(async () => {
   try {
-    if (userRole.toLowerCase() === 'estudiante') {
-      const response = await axios.get('http://localhost:3000/applications/list-vacants/by-user/' + userId);
-      console.log(response.data);
+    const getUser = await axios.get('http://localhost:3000/users/user/' + usuarioId);
+    if (getUser.data && getUser.data.role.toLowerCase() === 'estudiante') {
+      const response = await axios.get('http://localhost:3000/applications/list-vacants/by-user/' + usuarioId);
       vacantsArray.value = response.data;     
     }
-    if (userRole.toLowerCase() === 'reclutador') {
-      const response = await axios.get('http://localhost:3000/vacancies/list/' + userId);
-      console.log(response.data);
+    if (getUser.data && getUser.data.role.toLowerCase() === 'reclutador') {
+      const response = await axios.get('http://localhost:3000/vacancies/list/' + usuarioId);
       vacantsArray.value = response.data;  
     }
   } catch (error) {
